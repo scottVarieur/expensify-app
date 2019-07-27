17803e5c68049422e1bc5f6828217bb8d86bc305
unknown <scottvarieur@gmail.com>
2019-07-27 15:15:40 -0700

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,49 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import AppRouter from "./routers/AppRouter";
+import { Provider } from "react-redux";
+import configureStore from "./store/configureStore";
+import { addExpense } from "./actions/expenses";
+import { setTextFilter } from "./actions/filters";
+import getVisibleExpenses from "./selectors/expenses";
+import "normalize.css/normalize.css";
+import "./styles/styles.scss";
+import "react-dates/lib/css/_datepicker.css";
+
+const store = configureStore();
+
+store.subscribe(() => {
+  const state = store.getState();
+  const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+  console.log(visibleExpenses);
+});
+
+store.dispatch(
+  addExpense({
+    description: "Water bill",
+    amount: 4500
+  })
+);
+
+store.dispatch(
+  addExpense({
+    description: "Gas bill",
+    amount: 0,
+    createdAt: 1000
+  })
+);
+
+store.dispatch(
+  addExpense({
+    description: "rent",
+    amount: 109500
+  })
+);
+
+const jsx = (
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>
+);
+
+ReactDOM.render(jsx, document.getElementById("app"));
